ae07fdfd81fce9a22ffe2cdd71149379ba5ca003
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-01-02 15:34:47 +0700

    style: change default sans font to Rethink Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Rethink_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const rethink_sans = Rethink_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "KODEIN Boarding School",
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${rethink_sans.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
