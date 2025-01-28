531fc90875310f75a6289e3aecb33afc71e7b7b2
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2025-01-28 14:13:08 +0700

    feat: make responsive

--- src/components/Affiliate.tsx
+++ src/components/Affiliate.tsx
@@ -5,35 +5,33 @@ import { affiliates } from "@/constants/images";
 export default function Affiliate() {
   return (
     <>
-      <section className="flex justify-between container mx-auto px-14 pt-60">
+      <section className="flex flex-col xl:flex-row justify-between container mx-auto px-14 pt-60 gap-28">
         <div className="">
-          <h2 className="text-5xl font-extrabold text-gray-900 mb-4">
+          <h2 className="text-4xl md:text-5xl font-extrabold text-gray-900 mb-4">
             Afiliasi Kami
           </h2>
-          <p className="text-gray-400 text-xl font-thin max-w-3xl pr-10">
+          <p className="text-gray-400 text-justify text-lg md:text-xl  font-thin max-w-3xl">
             Afiliasi antara lembaga, memberikan siswa akses ke program tambahan,
             beasiswa, dan peluang pertukaran pelajar, yang memperluas wawasan
             dan jaringan mereka untuk masa depan.
           </p>
         </div>
-        <div className="shrink-0 flex gap-6 items-center [mask:linear-gradient(90deg,transparent,white_10%,white_80%,transparent)]">
+        <div className="shrink-0 flex gap-16 md:gap-6 items-center [mask:linear-gradient(90deg,transparent,white_10%,white_80%,transparent)]">
           {affiliates.map((affiliate, index) => (
             <Image key={index} {...affiliate} className="h-20 object-contain" />
           ))}
         </div>
       </section>
-      <section className="flex flex-row justify-between items-center container mx-auto px-14 py-64 gap-10">
+      <section className="flex flex-col md:flex-row justify-between items-center container mx-auto px-14 py-64 gap-20 md:gap-16">
         <iframe
-          width="620"
-          height="350"
           src="https://www.youtube.com/embed/TKXxJbiQpIE?si=VhYwyKnBTNmAOVyr"
-          className="rounded-3xl border-none"
+          className="flex-1 rounded-3xl border-none aspect-video size-full"
         />
-        <div>
-          <h2 className="text-5xl max-w-lg font-extrabold text-gray-900 mb-4">
+        <div className="flex-1">
+          <h2 className="text-4xl md:text-5xl max-w-lg font-extrabold text-gray-900 mb-4">
             Pentingnya Belajar IT di Era Modern
           </h2>
-          <p className="text-gray-400 text-xl font-thin max-w-2xl pr-10">
+          <p className="text-gray-400 text-justify text-lg md:text-xl font-thin max-w-2xl">
             Information Technology (IT) sangat penting di era modern karena
             mendukung efisiensi, inovasi, dan konektivitas. TI memungkinkan
             bisnis global, otomatisasi, dan analisis data, serta melindungi
