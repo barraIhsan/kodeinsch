73d25f03297836f8f52b181ccfd361edf6edf2d5
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2025-01-28 14:18:54 +0700

    style: remove justify text

--- src/components/Affiliate.tsx
+++ src/components/Affiliate.tsx
@@ -10,7 +10,7 @@ export default function Affiliate() {
           <h2 className="text-4xl md:text-5xl font-extrabold text-gray-900 mb-4">
             Afiliasi Kami
           </h2>
-          <p className="text-gray-400 text-justify text-lg md:text-xl  font-thin max-w-3xl">
+          <p className="text-gray-400 text-lg md:text-xl  font-thin max-w-3xl">
             Afiliasi antara lembaga, memberikan siswa akses ke program tambahan,
             beasiswa, dan peluang pertukaran pelajar, yang memperluas wawasan
             dan jaringan mereka untuk masa depan.
@@ -22,7 +22,7 @@ export default function Affiliate() {
           ))}
         </div>
       </section>
-      <section className="flex flex-col md:flex-row justify-between items-center container mx-auto px-14 py-64 gap-20 md:gap-16">
+      <section className="flex flex-col xl:flex-row justify-between items-center container mx-auto px-14 py-64 gap-20 md:gap-16">
         <iframe
           src="https://www.youtube.com/embed/TKXxJbiQpIE?si=VhYwyKnBTNmAOVyr"
           className="flex-1 rounded-3xl border-none aspect-video size-full"
@@ -31,7 +31,7 @@ export default function Affiliate() {
           <h2 className="text-4xl md:text-5xl max-w-lg font-extrabold text-gray-900 mb-4">
             Pentingnya Belajar IT di Era Modern
           </h2>
-          <p className="text-gray-400 text-justify text-lg md:text-xl font-thin max-w-2xl">
+          <p className="text-gray-400 text-lg md:text-xl font-thin max-w-2xl">
             Information Technology (IT) sangat penting di era modern karena
             mendukung efisiensi, inovasi, dan konektivitas. TI memungkinkan
             bisnis global, otomatisasi, dan analisis data, serta melindungi
