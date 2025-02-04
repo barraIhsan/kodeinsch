6175de55ef5768099af70c2eef9fedeed4ed97a9
Mulky Ayyubi <170484966+MulkyAyyubi@users.noreply.github.com>
2025-02-04 14:12:34 +0700

    style: change paddings and gaps

--- src/components/Affiliate.tsx
+++ src/components/Affiliate.tsx
@@ -5,7 +5,7 @@ import { affiliates } from "@/constants/images";
 export default function Affiliate() {
   return (
     <>
-      <section className="flex flex-col xl:flex-row justify-between container mx-auto px-14 pt-60 gap-28">
+      <section className="flex flex-col xl:flex-row justify-between container mx-auto px-5 sm:px-12 pt-32 xl:pt-60 gap-16 xl:gap-28">
         <div className="">
           <h2 className="text-4xl md:text-5xl font-extrabold text-gray-900 mb-4">
             Afiliasi Kami
@@ -22,7 +22,7 @@ export default function Affiliate() {
           ))}
         </div>
       </section>
-      <section className="flex flex-col xl:flex-row justify-between items-center container mx-auto px-14 py-64 gap-20 md:gap-16">
+      <section className="flex flex-col xl:flex-row justify-between items-center container mx-auto px-5 sm:px-12 pb-12 xl:pb-64 pt-40 xl:pt-64 gap-20 md:gap-16">
         <iframe
           src="https://www.youtube.com/embed/TKXxJbiQpIE?si=VhYwyKnBTNmAOVyr"
           className="flex-1 rounded-3xl border-none aspect-video size-full"
