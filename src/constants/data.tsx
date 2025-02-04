f130f31de197ede5734ac95be6921ddebf848303
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-02-04 17:29:08 +0700

    feat: add roadmap section

--- src/constants/data.tsx
+++ src/constants/data.tsx
@@ -87,6 +87,67 @@ export const programs: Array<{
   },
 ];
 
+export const roadmaps: Array<Array<Array<{ title: string; desc: string }>>> = [
+  [
+    [
+      {
+        title: "Komputer Dasar",
+        desc: "Microsoft Word, Microsoft Excel, dan Microsoft Power Point",
+      },
+      {
+        title: "UI/UX",
+        desc: "Figma",
+      },
+    ],
+    [
+      {
+        title: "Front-end",
+        desc: "HTML/CSS",
+      },
+      {
+        title: "Web Framework",
+        desc: "TailwindCSS, ui/shadcn, Vite, React, dan Next.js",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        title: "Back-end",
+        desc: "NodeJS, Express.js",
+      },
+      {
+        title: "Multimedia",
+        desc: "Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro, Adobe After Effects, dan Blender",
+      },
+    ],
+    [
+      {
+        title: "Mobile Apps",
+        desc: "Dart dan Flutter",
+      },
+      {
+        title: "Sertifikasi",
+        desc: "Project",
+      },
+    ],
+  ],
+  [
+    [
+      {
+        title: "Sertifikasi",
+        desc: "Project",
+      },
+    ],
+    [
+      {
+        title: "Sertifikasi",
+        desc: "Project",
+      },
+    ],
+  ],
+];
+
 export const footerLinks: Array<{
   title: string;
   links: Array<{
