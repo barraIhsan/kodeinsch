9deb1e7dc98696e90bba5379316ab394068e8b08
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-02-11 18:20:58 +0700

    refactor: move about logo into global logo dir

--- src/constants/data.tsx
+++ src/constants/data.tsx
@@ -94,25 +94,45 @@ export const programs: Array<{
 
 export const curriculums: Array<{
   title: string;
+  desc?: string;
   logo: string;
-  list: Array<string>
+  list: Array<string>;
 }> = [
   {
     title: "Materi Diniyah",
-    logo: "/img/about/logoDiniyah.svg",
-    list: ["Tauhid","Tahfidz","Tafsir","Hadist","Fiqih","Siroh",]
+    logo: "/img/logo/moon.svg",
+    list: ["Tauhid", "Tahfidz", "Tafsir", "Hadist", "Fiqih", "Siroh"],
   },
   {
-    title: "Materi IT(Information Technology)",
-    logo: "/img/about/logoCode.svg",
-    list: ["Web Programming","Mobile Programming","UI/UX","Database","Graphic Design","Motion Graphic","Video Editing","Networking","IoT",]
+    title: "Materi IT",
+    desc: "(Information Technology)",
+    logo: "/img/logo/code.svg",
+    list: [
+      "Web Programming",
+      "Mobile Programming",
+      "UI/UX",
+      "Database",
+      "Graphic Design",
+      "Motion Graphic",
+      "Video Editing",
+      "Networking",
+      "IoT",
+    ],
   },
   {
     title: "Materi Diknas",
-    logo: "/img/about/logoKemendik.png",
-    list: ["Matematika","Bahasa Indonesia","Bahasa Inggris","IPA Terpadu","IPS Terpadu","PKN","Try Out UTBK",]
+    logo: "/img/logo/diknas.svg",
+    list: [
+      "Matematika",
+      "Bahasa Indonesia",
+      "Bahasa Inggris",
+      "IPA Terpadu",
+      "IPS Terpadu",
+      "PKN",
+      "Try Out UTBK",
+    ],
   },
-]
+];
 
 export const roadmaps: Array<Array<Array<{ title: string; desc: string }>>> = [
   [
