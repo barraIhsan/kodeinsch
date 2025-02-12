84e03efe786be2755280de4b05958d5d933d60b5
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-02-12 09:39:16 +0700

    refactor: create floating whatsapp button comps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,8 @@ import About from "@/app/About";
 import Curriculum from "@/app/Curriculum";
 import Roadmap from "@/app/Roadmap";
 import Testimonial from "@/components/Testimonial";
-import { SiWhatsapp } from "@icons-pack/react-simple-icons";
-import Link from "next/link";
 import ColoredSection from "@/components/ColoredSection";
+import FloatingButton from "@/components/FloatingButton";
 
 export default function Home() {
   return (
@@ -37,14 +36,7 @@ export default function Home() {
       </ColoredSection>
 
       <Footer />
-      <div className="fixed bottom-10 right-10 size-16 bg-[#25D366]/50 rounded-full flex justify-center items-center animate-ping" />
-      <Link
-        className="fixed bottom-10 right-10 size-16 flex justify-center items-center rounded-full bg-[#25D366] z-40"
-        href="https://wa.me/6282170270241"
-        target="_blank"
-      >
-        <SiWhatsapp className="fill-white size-8" />
-      </Link>
+      <FloatingButton />
     </>
   );
 }
