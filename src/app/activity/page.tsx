acbb59b24d88600049f186e922dd4b5161202a9f
Zawindra <zawindra@gmail.com>
2025-02-19 14:36:57 +0700

    feat: create activity page
    Add hero section, and one program section.

---
+++ src/app/activity/page.tsx
@@ -0,0 +1,22 @@
+import ColoredSection from "@/components/ColoredSection";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import HeroActivity from "./Hero";
+import ProgramActivity from "./Program";
+
+export default function page() {
+  return (
+    <>
+      <Navbar />
+      <ColoredSection
+        className="from-[#FF9D2D] to-[#EF7E30] mt-24"
+        left="-left-72 bottom-20"
+        right="-right-52 -top-16"
+      >
+        <HeroActivity />
+      </ColoredSection>
+      <ProgramActivity />
+      <Footer />
+    </>
+  );
+}
