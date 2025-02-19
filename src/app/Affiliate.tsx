d67ddd0b20a0946d31c4293e6e814552da7295b7
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-02-19 11:54:49 +0700

    style: add decorative border on homepage

--- src/app/Affiliate.tsx
+++ src/app/Affiliate.tsx
@@ -6,22 +6,28 @@ import { Title } from "@/components/Text";
 export default function Affiliate() {
   return (
     <>
-      <section className="flex flex-col xl:flex-row justify-between container mx-auto px-5 sm:px-12 pt-32 xl:pt-60 gap-16 xl:gap-28">
-        <div className="space-y-4">
-          <Title>Afiliasi Kami</Title>
-          <p className="text-gray-500 text-lg md:text-xl  font-thin max-w-3xl">
-            Afiliasi antara lembaga, memberikan siswa akses ke program tambahan,
-            beasiswa, dan peluang pertukaran pelajar, yang memperluas wawasan
-            dan jaringan mereka untuk masa depan.
-          </p>
-        </div>
-        <div className="shrink-0 flex gap-16 md:gap-6 items-center [mask:linear-gradient(90deg,transparent,white_10%,white_80%,transparent)] w-fit overflow-hidden">
-          {affiliates.map((affiliate, index) => (
-            <Image key={index} {...affiliate} className="h-20 object-contain" />
-          ))}
+      <section className="border-x border-gray-200 container mx-auto pt-32 xl:pt-44 gap-16 xl:gap-28">
+        <div className="border-y border-gray-200 flex flex-col xl:flex-row justify-between px-5 sm:px-12 py-20">
+          <div className="space-y-4">
+            <Title>Afiliasi Kami</Title>
+            <p className="text-gray-500 text-lg md:text-xl  font-thin max-w-3xl">
+              Afiliasi antara lembaga, memberikan siswa akses ke program
+              tambahan, beasiswa, dan peluang pertukaran pelajar, yang
+              memperluas wawasan dan jaringan mereka untuk masa depan.
+            </p>
+          </div>
+          <div className="shrink-0 flex gap-16 md:gap-6 items-center [mask:linear-gradient(90deg,transparent,white_10%,white_80%,transparent)] w-fit overflow-hidden">
+            {affiliates.map((affiliate, index) => (
+              <Image
+                key={index}
+                {...affiliate}
+                className="h-20 object-contain"
+              />
+            ))}
+          </div>
         </div>
       </section>
-      <section className="flex flex-col xl:flex-row justify-between items-center container mx-auto px-5 sm:px-12 pb-12 xl:pb-64 pt-40 xl:pt-64 gap-20 md:gap-16">
+      <section className="border-x border-gray-200 flex flex-col xl:flex-row justify-between items-center container mx-auto px-5 sm:px-12 py-32 gap-20 md:gap-16">
         <iframe
           src="https://www.youtube.com/embed/ibJ0lpj1a0E"
           className="flex-1 rounded-3xl border-none aspect-video size-full"
--- src/app/Program.tsx
+++ src/app/Program.tsx
@@ -9,9 +9,11 @@ export default function Program() {
   return (
     <section
       id="program"
-      className="container mx-auto grid gap-14 px-4 lg:pt-12 pb-12 lg:pb-36 sm:px-8 lg:px-0"
+      className="border-x border-gray-200 container mx-auto grid gap-14 px-4 lg:pt-12 pb-12 lg:pb-36 sm:px-8 lg:px-0"
     >
-      <Title className="text-center">Program Unggulan</Title>
+      <Title className="text-center border-y border-gray-200">
+        Program Unggulan
+      </Title>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {programs.map((program, index) => (
           <div
