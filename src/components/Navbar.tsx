fa92b8606938e73fb9260ba4d6fcc073cd5fd236
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-02-20 11:05:59 +0700

    style(navbar): always draw the bottom border

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
@@ -24,21 +24,10 @@ import { leftMenus, rightMenus } from "@/constants/data";
 
 export default function Navbar() {
   const [navMobileOpen, setNavMobileOpen] = useState(false);
-  const [scrollY, setScrollY] = useState(0);
 
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      setScrollY(window.scrollY);
-    });
-  }, []);
   return (
     <>
-      <nav
-        className={cn(
-          `bg-white fixed top-0 border-gray-200 z-50 inset-x-0`,
-          scrollY == 0 ? "" : "border-b",
-        )}
-      >
+      <nav className="bg-white fixed top-0 border-b border-gray-200 z-50 inset-x-0">
         <div className="container flex justify-between items-center px-5 md:px-12 py-5 mx-auto">
           <div className="flex items-center gap-4">
             <Link href="/">
