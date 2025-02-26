cc0bc49fa5afa6d1802b1626e0fcf233fd6a02aa
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-02-26 14:25:43 +0700

    feat: rework activity section

---
+++ src/data/activity.tsx
@@ -0,0 +1,69 @@
+export interface activity {
+  src: string;
+  title: string;
+  desc: string;
+}
+
+export const studyTour: Array<activity> = [
+  {
+    src: "ccit.png",
+    title: "Kunjungan CCIT-FTUI",
+    desc: "Program dua bulan di Pare Kampung Inggris mengajarkan Bahasa Inggris lewat imersi, membangun kepercayaan diri siswa untuk berinteraksi dalam dunia profesional dan sosial digital.",
+  },
+  {
+    src: "perpusnas.png",
+    title: "Kunjungan PerpusNas",
+    desc: "Program dua bulan di Pare Kampung Inggris mengajarkan Bahasa Inggris lewat imersi, membangun kepercayaan diri siswa untuk berinteraksi dalam dunia profesional dan sosial digital.",
+  },
+  {
+    src: "polbis.png",
+    title: "Seminar POLBIS",
+    desc: "Program dua bulan di Pare Kampung Inggris mengajarkan Bahasa Inggris lewat imersi, membangun kepercayaan diri siswa untuk berinteraksi dalam dunia profesional dan sosial digital.",
+  },
+];
+
+export const classMeet: Array<activity> = [
+  {
+    src: "club.png",
+    title: "Club",
+    desc: "Club ini dibuat untuk penguasaan keahlian. Programming dan Multimedia Club memungkinkan siswa mengeksplorasi lebih jauh materi kelas, memperkuat minat, dan keahlian mereka.",
+  },
+  {
+    src: "koching.png",
+    title: "Koching",
+    desc: "Program ini bertujuan untuk mempermudah siswa dalam mengerjakan tugas / project dengan cara konsultasi kepada mentor atau musyrif dalam bidangnya",
+  },
+  {
+    src: "ekstrakulikuler.png",
+    title: "Ekstrakulikuler",
+    desc: "Di samping kegiatan akademik, siswa juga dapat mengikuti futsal dan basket untuk menjaga kebugaran. Kegiatan ini penting untuk melengkapi keterampilan mereka di bidang digital.",
+  },
+];
+
+export const musabaqah: Array<activity> = [
+  {
+    src: "tahfidz.png",
+    title: "Tahfidz Qur'an",
+    desc: "Program Tahfidz Qur’an mendukung siswa dalam menghafal dan memahami makna Al-Qur’an dengan bimbingan terstruktur, memperkuat hafalan dan nilai-nilai islami.",
+  },
+  {
+    src: "kajian.png",
+    title: "Kajian Islam",
+    desc: "Program Tahfidz Qur’an mendukung siswa dalam menghafal dan memahami makna Al-Qur’an dengan bimbingan terstruktur, memperkuat hafalan dan nilai-nilai islami.",
+  },
+  {
+    src: "tryout.png",
+    title: "Tryout Bulanan",
+    desc: "Program Tahfidz Qur’an mendukung siswa dalam menghafal dan memahami makna Al-Qur’an dengan bimbingan terstruktur, memperkuat hafalan dan nilai-nilai islami.",
+  },
+  {
+    src: "sertifikat.png",
+    title: "Sertifikat",
+    desc: "Program Tahfidz Qur’an mendukung siswa dalam menghafal dan memahami makna Al-Qur’an dengan bimbingan terstruktur, memperkuat hafalan dan nilai-nilai islami.",
+  },
+  {
+    src: "ldks.png",
+    title: "LDKS",
+    desc: "Program Tahfidz Qur’an mendukung siswa dalam menghafal dan memahami makna Al-Qur’an dengan bimbingan terstruktur, memperkuat hafalan dan nilai-nilai islami.",
+  },
+];
