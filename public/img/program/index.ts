860f38eaae09240d39d3210c9dfd11614ea5ac28
Zawindra <zawindra@gmail.com>
2025-03-11 10:40:42 +0700

    chore: add image for academic page

---
+++ public/img/program/index.ts
@@ -0,0 +1,14 @@
+import qurancamp from "./qurancamp.png";
+import englishcamp from "./englishcamp.png";
+import incubator from "./incubator.png";
+import club from "./club.png";
+import koching from "./koching.png";
+import ekstrakulikuler from "./extrakulikuler.png";
+import tahfidzquran from "./tahfidzquran.png";
+import kajianislam from "./kajian.png";
+import tryoutbulanan from "./tryout.png";
+import sertifikat from "./sertifikat.png";
+import ldks from "./ldks.png";
+
+export { qurancamp,englishcamp,incubator,club,koching,ekstrakulikuler,tahfidzquran,kajianislam,tryoutbulanan,sertifikat,ldks };
+
